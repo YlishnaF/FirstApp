c2a1a6777fd99b252d9489a95bc7b351d79e58ed
YlishnaF <ylishnaf@yandex.ru>
2022-06-30 13:12:54 +0300

    четверное дз

--- app/src/main/java/com/example/firstapp/ui/MainActivity.java
+++ app/src/main/java/com/example/firstapp/ui/MainActivity.java
@@ -3,12 +3,17 @@ package com.example.firstapp.ui;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CalendarView;
+import android.widget.CheckBox;
+import android.widget.RadioButton;
+import android.widget.Switch;
 import android.widget.TextView;
 
 import com.example.firstapp.R;
@@ -23,15 +28,57 @@ import java.util.Map;
 public class MainActivity extends AppCompatActivity implements CalculatorView {
     private TextView resultTxt;
     private CalculatorPresenter presenter;
+    private RadioButton checkBox_dark;
+    private RadioButton checkBox_light;
+    SharedPreferences preferences;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        preferences = getSharedPreferences("themes.xml", Context.MODE_PRIVATE);
+
+        int theme = preferences.getInt("theme", R.style.AppTheme);
+
+        setTheme(theme);
+
         setContentView(R.layout.activity_main);
         presenter = new CalculatorPresenter(this, new Calculator());
 
         resultTxt = findViewById(R.id.result);
+
+        checkBox_dark = findViewById(R.id.night_theme);
+        checkBox_light =findViewById(R.id.light_theme);
+
+        if (checkBox_dark != null) {
+            checkBox_dark.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+
+                    preferences.edit()
+                            .putInt("theme", R.style.NightAppTheme)
+                            .commit();
+                    checkBox_light=null;
+
+                    recreate();
+                }
+            });
+        }
+
+        if (checkBox_light != null) {
+            checkBox_light.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    preferences.edit()
+                            .putInt("theme", R.style.AppTheme)
+                            .commit();
+                    recreate();
+                    checkBox_dark=null;
+
+                }
+            });
+        }
         presenter = new CalculatorPresenter(this, new Calculator());
 
         final Map<Integer, Integer> numbers = new HashMap<>();
@@ -86,6 +133,8 @@ public class MainActivity extends AppCompatActivity implements CalculatorView {
                 presenter.onDotPressed();
             }
         });
+
+
     }
 
     @Override
